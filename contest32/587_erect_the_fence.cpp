1a98c50aa544e7134b6157bc5772d98dceb56e5f
Lu Yi <435977170@qq.com>
2017-05-15 22:54:01 +0800

    luyi at 6011 upload in 2017/05/15, add my algorithm implement of find hull

--- contest32/587_erect_the_fence.cpp
+++ contest32/587_erect_the_fence.cpp
@@ -85,3 +85,91 @@ public:
         return ret;
     }
 };
+
+/**
+ * Definition for a point.
+ * struct Point {
+ *     int x;
+ *     int y;
+ *     Point() : x(0), y(0) {}
+ *     Point(int a, int b) : x(a), y(b) {}
+ * };
+ */
+class Solution {
+private:
+    vector<Point> ret;
+
+    double findDist(Point& A, Point& B, Point& C){
+        //(y2 - y1)*x + (x1 - x2)*y + x2*y1 - x1*y2 = 0
+        return abs((B.y - A.y)*C.x + (A.x - B.x)*C.y + B.x*A.y - A.x*B.y) / sqrt((B.y - A.y)*(B.y - A.y) + (A.x - B.x)*(A.x - B.x));
+    }
+
+    int findDirection(Point& A, Point& B, Point& C){
+        //AB and AC
+        return (B.x - A.x)*(C.y - A.y) - (B.y - A.y)*(C.x - A.x);
+    }
+
+    void findHull(vector<Point>& set, Point &A, Point &B){
+        int lens = set.size();
+        if(lens==0) return;
+
+        int idx = 0;
+        double max_dist = findDist(A, B, set[0]);
+        for(int i=1;i<lens;i++){
+            double dist = findDist(A, B, set[i]);
+            if(dist>max_dist){
+                max_dist = dist;
+                idx = i;
+            }
+        }
+        Point C = set[idx];
+        ret.push_back(C);
+
+        vector<Point> up, down;
+        for(int i=0;i<lens;i++){
+            if(i!=idx){
+                int tmp = findDirection(A,C,set[i]);
+                /*if(tmp>0)   up.push_back(set[i]);
+                else if(tmp < 0){
+                    int tmp_x = findDirection(C,B,set[i]);
+                    if(tmp_x>0) down.push_back(set[i]);
+                }*/
+                if(tmp>=0)   up.push_back(set[i]);
+                else{
+                    int tmp_x = findDirection(C,B,set[i]);
+                    if(tmp_x>=0) down.push_back(set[i]);
+                }
+            }
+        }
+
+        findHull(up,A,C);
+        findHull(down,C,B);
+    }
+public:
+    static bool cmp(Point a, Point b){
+        if(a.x==b.x)    return a.y < b.y;
+        return a.x < b.x;
+    }
+
+    vector<Point> outerTrees(vector<Point>& points) {
+        int lens = points.size();
+        if(lens<=1) return points;
+        sort(points.begin(), points.end(), cmp);
+
+        Point A = points[0], B = points.back();
+        ret.push_back(A); ret.push_back(B);
+        vector<Point> up, down, online;
+        for(int i=1;i<lens-1;i++){
+            int tmp = findDirection(A,B,points[i]);
+            if(tmp==0)  online.push_back(points[i]);
+            else if(tmp > 0)    up.push_back(points[i]);
+            else    down.push_back(points[i]);
+        }
+        if(up.empty()||down.empty()){
+            ret.insert(ret.end(),online.begin(),online.end());
+        }
+        findHull(up, A, B);
+        findHull(down, B, A);
+        return ret;
+    }
+};
